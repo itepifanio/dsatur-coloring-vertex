40de3f5dd6e7a7f1b9b9bc78954fb50f8a17544d
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2021-03-30 08:57:02 -0300

    Grafo de 5 cores e melhoria no print

--- src/main.cpp
+++ src/main.cpp
@@ -8,55 +8,100 @@
 #include <chrono>
 #include <map>
 
-int main(int argc, char** argv)
+std::vector<Vertex *> test(int n, fort::char_table &table)
+{
+    std::vector<Vertex *> vertexes;
+    int edges = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        Vertex *v = new Vertex(i + 1);
+
+        vertexes.push_back(v);
+    }
+
+    std::srand( ( unsigned int )std::time( nullptr ) );
+
+    for (int j = 0; j < std::rand() % 10000; ++j)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            int adjacent = i;
+            while (adjacent == i)
+            {
+                adjacent = std::rand() % (n - 1);
+            }
+
+            edges++;
+            vertexes[i]->addAdjacentVertex(vertexes[adjacent]);
+        }
+    }
+
+    table << edges;
+
+    return vertexes;
+}
+
+void print(std::string nameFile, int vertex, int chromaticNumber, int edges, fort::char_table &table)
 {
     Reader r;
+    std::vector<Vertex *> v = r.readGraphFromFile("./our_graphs/"+nameFile, chromaticNumber, vertex);
 
+    table << nameFile;
+    table << vertex;
+    table << edges;
+    
+    Graph *g = new (std::nothrow) Graph(v);
+    auto start = std::chrono::high_resolution_clock::now();
+    g->brown();
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    if (g->hasDsaturWorked())
+    {
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        table << g->getTotalColors();
+        table << duration.count() << fort::endr;
+        // g->printGraph();
+    }
+    else
+    {
+        std::cout << "Coloring doesnt worked. Debug the graph:" << std::endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
     std::map<std::string, int> files = {
         {"3k_graph.txt", 0},
         {"4k_graph.txt", 1},
+        {"5k_graph.txt", 2},
     };
 
-    int chromaticNumbers[2] = {3, 4};
-    int vertexesNumbers[2] = {7, 7};
-    int edges[2] = {11, 18};
-    
+    int chromaticNumbers[3] = {3, 4, 5};
+    int vertexesNumbers[3] = {7, 7, 5};
+    int edges[3] = {11, 18, 10};
+
+    fort::char_table table;
+    table << fort::header
+            << "Graph"
+            << "Vertexes"
+            << "Edges"
+            << "Colors"
+            << "Milisseconds" << fort::endr;
+
     if(argc == 2) 
     {
         std::string nameFile(argv[1]);
         int i = files[nameFile];
-        std::vector<Vertex *> v = r.readGraphFromFile("./our_graphs/"+nameFile, chromaticNumbers[i], vertexesNumbers[i]);
-        fort::char_table table;
-        table << fort::header
-                << "Graph"
-                << "Vertexes"
-                << "Edges"
-                << "Colors"
-                << "Milisseconds" << fort::endr;
-
-        table << nameFile;
-        table << vertexesNumbers[i];
-        table << edges[i];
-        
-        Graph *g = new (std::nothrow) Graph(v);
-        auto start = std::chrono::high_resolution_clock::now();
-        g->brown();
-        auto stop = std::chrono::high_resolution_clock::now();
-
-        if (g->hasDsaturWorked())
-        {
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            table << g->getTotalColors();
-            table << duration.count() << fort::endr;
-            // g->printGraph();
+        print(nameFile, vertexesNumbers[i], chromaticNumbers[i], edges[i], table);
+    } else {
+        Reader r;
+        for (std::map<std::string,int>::iterator it=files.begin(); it!=files.end(); ++it) {
+            print(it->first, vertexesNumbers[it->second], chromaticNumbers[it->second], edges[it->second], table);
         }
-        else
-        {
-            std::cout << "Coloring doesnt worked. Debug the graph:" << std::endl;
-        }
-
-        std::cout << table.to_string() << std::endl;
     }
+
+    std::cout << table.to_string() << std::endl;
 
     return 0;
 }
